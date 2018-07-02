d955c5ecde9ff93c6d794b576ed55bdb6a0fd54b
lulongji <lulongji2011@163.com>
2018-07-02 15:40:45 +0800

    初始化

---
+++ src/main/java/com/example/demo/web/TestController.java
@@ -0,0 +1,20 @@
+package com.example.demo.web;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: Hello World
+ * @Author: lulongji
+ */
+@RestController
+@EnableAutoConfiguration
+public class TestController {
+
+    @RequestMapping("/")
+    public String test() {
+        return "Hello world";
+    }
+}
