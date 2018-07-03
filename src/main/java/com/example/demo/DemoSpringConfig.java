52736c3a958360fc18ecae75af9838656b496d56
lulongji <lulongji2011@163.com>
2018-07-03 18:20:20 +0800

    初始化

--- src/main/java/com/example/demo/DemoSpringConfig.java
+++ src/main/java/com/example/demo/DemoSpringConfig.java
@@ -62,8 +62,7 @@ public class DemoSpringConfig extends WebMvcConfigurationSupport {
      **/
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        //将所有/static/** 访问都映射到classpath:/static/ 目录下
-        //registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 
     /**
