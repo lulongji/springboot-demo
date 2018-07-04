6c88f94058d4f6849771fe50d99a7837d9b34bc7
lulongji <lulongji2011@163.com>
2018-07-04 11:16:43 +0800

    初始化

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,13 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableCaching //开启缓存
+@EnableTransactionManagement // 开启事务管理
 @MapperScan("com.example.demo.dao")
 @ImportResource(locations = {"classpath:spring/spring-redis.xml"})
 public class DemoApplication {
