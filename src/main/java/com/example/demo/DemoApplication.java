f25f656c7bc47295e5631fb9a72edde3743b02e1
lulongji <lulongji2011@163.com>
2018-07-04 10:44:15 +0800

    初始化

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @MapperScan("com.example.demo.dao")
+@ImportResource(locations = {"classpath:spring/spring-redis.xml"})
 public class DemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
 
+import com.llj.base.page.Result;
+import com.llj.base.utils.cache.redis.springTemplate.RedisTemplateUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Description: Hello World
@@ -21,6 +25,31 @@ public class TestController {
     @RequestMapping("/")
     public String test() {
         return path;
+    }
+
+    @RequestMapping("/setRedis")
+    public Result setRedis(String name) {
+        Result result = Result.success();
+        try {
+            RedisTemplateUtil.set("springboot-test" + name, name, 5, TimeUnit.MINUTES);
+        } catch (Exception e) {
+            result = Result.failure();
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @RequestMapping("/getRedis")
+    public Result getRedis(String name) {
+        Result result = Result.success();
+        try {
+            Object obj = RedisTemplateUtil.get("springboot-test" + name);
+            result.setResult(obj);
+        } catch (Exception e) {
+            result = Result.failure();
+            e.printStackTrace();
+        }
+        return result;
     }
 
 }
