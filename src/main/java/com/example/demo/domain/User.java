c440847c20b057147a43d6bba6dc11a741f3d819
lulongji <lulongji2011@163.com>
2018-07-09 10:50:22 +0800

    初始化

--- src/main/java/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/domain/User.java
@@ -1,45 +1,18 @@
 package com.example.demo.domain;
 
+import lombok.Data;
+
 /**
  * @Description:
  * @Author: lulongji
  */
+@Data
 public class User {
+
 
     private String id;
     private String userName;
     private String password;
     private Integer age;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
